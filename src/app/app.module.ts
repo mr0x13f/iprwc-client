e44bdb814f6cb7e9e7a389822912b98e40314824
0x13f <mr0x13f@gmail.com>
2019-12-31 17:31:41 +0100

    renamed ProductsComponent to HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,18 @@ import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { OrdersComponent } from './orders/orders.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
+  { path: "",           component: HomeComponent },
   { path: "login",      component: LoginComponent },
   { path: "register",   component: RegisterComponent },
   { path: "user",       component: UserComponent },
   { path: "admin",      component: AdminComponent },
-  { path: "",           component: ProductsComponent },
   { path: "wishlist",   component: WishlistComponent },
   { path: "cart",       component: CartComponent },
   { path: "orders",     component: OrdersComponent },
@@ -31,7 +31,7 @@ const appRoutes: Routes = [
     CartComponent,
     WishlistComponent,
     OrdersComponent,
-    ProductsComponent,
+    HomeComponent,
     UserComponent,
     AdminComponent,
     LoginComponent,
